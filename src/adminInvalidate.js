62563eb9048756bb4938af97f6256f9c39369188
Mohamed Honsy <mohamed.h.alzanaty@gmail.com>
2025-08-14 19:18:33 +0300

    Connect to db in new lambda

--- src/adminInvalidate.js
+++ src/adminInvalidate.js
@@ -1,14 +1,23 @@
 "use strict";
 
 const { logMessage } = require("./common/logger");
+const { connectDB } = require("./config/db");
 const { invalidateGlobalToken } = require("./services/quickbooksService");
 
 // Lambda handler to invalidate the global QuickBooks token.
 // Invoke from AWS Console and pass event like: { "mode": "expire|refreshNow|revoke" }
+let dbInitialized = false;
+
 exports.handler = async (event = {}) => {
   const mode = (event && event.mode) || process.env.INVALIDATE_MODE || "expire";
 
   try {
+    if (!dbInitialized) {
+      logMessage("INFO", "[adminInvalidate] Initializing DB connection (cold start)");
+      await connectDB();
+      dbInitialized = true;
+    }
+    
     logMessage("INFO", "🔄 Admin invalidate token invoked", { mode });
     const result = await invalidateGlobalToken(mode);
     
